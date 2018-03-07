b457d40c550d2502bea2f3ee52fdcd2550e257c0
zincalati <iamtylerlouie@gmail.com>
2018-03-07 13:44:57 -0800

    Update skill_enabler_script.js

--- skill_enabler_script.js
+++ skill_enabler_script.js
@@ -23,12 +23,4 @@ function fillForm() {
           });
 }
 
-if (window.jQuery === undefined) {
-    var script    = document.createElement( 'script' );
-    script.src    = 'https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js';
-    script.onload = function() { fillForm; }
-    document.body.appendChild(script);
-}
-else {
-    fillForm();
-}
+fillForm();
